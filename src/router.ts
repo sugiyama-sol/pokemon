7586a82d73ca806022d38a543470a5e3f080ade0
杉山敏明 <frusion.mail@gmail.com>
2018-12-03 17:32:41 +0900

    first commit

--- src/router.ts
+++ src/router.ts
@@ -1,24 +1,22 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
+import List from "./views/List.vue";
+import Detail from "./views/Detail.vue";
 
 Vue.use(Router);
 
 export default new Router({
+  mode: "history",
   routes: [
     {
       path: "/",
-      name: "home",
-      component: Home
+      name: "list",
+      component: List
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+      path: "/detail",
+      name: "detail",
+      component: Detail
     }
   ]
 });
